3557757867fd8bc30c0bdcf200c23855d202e872
brahim <139687519+rajelmn@users.noreply.github.com>
2024-07-01 17:45:12 +0000

    Update script.js
    adding the local Storage

--- script.js
+++ script.js
@@ -1,12 +1,35 @@
+
 let todo = document.getElementById('to-do');
 let btn = document.getElementById('btn');
 let inputvalue = document.getElementById('input-box')
 let ul = document.querySelector('ul')
-btn.onclick = function () {
+
+let arr = []
+let i = 0;
+
+btn.addEventListener('click', () => {
+    if(inputvalue.value) {
    let li = document.createElement('li');
    let input = document.createElement('input');
    input.type = 'checkbox';
    let text = document.createElement('span');
    text.innerHTML = inputvalue.value;
-   ul.appendChild(li).append(input, text)
-};
+   ul.appendChild(li).append(input, text);
+   arr.push(text.innerHTML)
+   inputvalue.value = ''
+   
+   arr.forEach( (a) => localStorage.setItem(`value${i++}`, a) )
+  }
+
+})
+
+window.addEventListener('load', () => {
+     for(let i = 0; i < localStorage.length; i++) {
+          let li = document.createElement('li');
+          let span = document.createElement('span');
+          let input = document.createElement('input')
+          input.type = 'checkbox';
+          span.textContent = localStorage.getItem(`value${i}`);
+          ul.appendChild(li).append(input, span)
+     }
+})
